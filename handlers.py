d78a9fe0e287f22be6c167b5e25868f367e8bd21
HUSSEIN <167720962+ihhaiq@users.noreply.github.com>
2026-08-07 23:41:09 +0300

    Import BTN_VINYL_ROSE alongside BTN_VINYL_BLOODY

--- handlers.py
+++ handlers.py
@@ -25,8 +25,7 @@ import texts as texts_module
 from texts import clean_html, text_to_bold, text_to_italic, text_to_code, text_to_underline, text_to_strikethrough
 import custom_texts
 import math
-from texts import BTN_VINYL_BLOODY
-BTN_VINYL_ROSE
+from texts import BTN_VINYL_BLOODY , BTN_VINYL_ROSE
 logger = logging.getLogger(__name__)
 router = Router()
 
