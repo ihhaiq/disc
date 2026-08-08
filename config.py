4c54bbeb795ea233af121102ca46393d1b6839b3
HUSSEIN <167720962+ihhaiq@users.noreply.github.com>
2026-08-08 21:50:42 +0300

    اضافة القالب الجديد الفضي

--- config.py
+++ config.py
@@ -57,6 +57,11 @@ VINYL_BLOODY_PATH = os.path.join(ASSETS_DIR, "vinyl_bloody.png")
 VINYL_ROSE_PATH = os.path.join(ASSETS_DIR, "vinyl_rose.png")
 SHADOW_ROSE_PATH = os.path.join(ASSETS_DIR, "shadow_rose.png")
 VINYL_EMERALD_PATH = os.path.join(ASSETS_DIR, "vinyl_emerald.png")
+
+FRAME_CLASSIC_PATH = os.path.join(ASSETS_DIR, "frame_classic.png")
+FRAME_CLASSIC_LABEL_RATIO = 0.74  # قطر صورة الغلاف / مقاس القرص (يغطي كامل فتحة الإطار)
+FRAME_CLASSIC_DISC_RATIO = 0.77   # قطر القرص الخلفي الغامق / مقاس القرص (يختفي بالكامل خلف الحلقة)
+
 TEMP_DIR = os.path.join(BASE_DIR, "temp")
 
 # --- مجلد التخزين الدائم (لازم يكون مربوط بـ Railway Volume) ---
